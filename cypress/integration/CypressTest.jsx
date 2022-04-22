6461ff3c55b5a2e798b2fbc00d11f2e3bfe1a821
Jere Nuora <jere.nuora@gmail.com>
2022-04-22 10:55:13 +0300

    ?

--- cypress/integration/CypressTest.jsx
+++ cypress/integration/CypressTest.jsx
@@ -6,4 +6,17 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+  it('pokemon page can be opened', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains(
+      'ivysaur'
+    )
+    cy.contains(
+      'overgrow'
+    )
+    cy.contains(
+      'chlorophyll'
+    )
+  })
 })
